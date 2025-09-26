6aa1523377b0ab995279477ec1c1e40d14c9feeb
Daniel Kubatko <dankubatko@gmail.com>
2025-09-25 18:23:48 -0700

    Enable batch processing of all cards

--- miniapp/src/hooks/index.ts
+++ miniapp/src/hooks/index.ts
@@ -4,3 +4,4 @@ export { useOrientation } from './useOrientation';
 export { useModal } from './useModal';
 export { useMainButton } from './useMainButton';
 export { useSwipeHandlers } from './useSwipeHandlers';
+export { useBatchLoader } from './useBatchLoader';
